ec8d8a00ebaf7d2d5846d99255d21ae707140560
Chang Ye <1020994740a@gmail.com>
2021-03-29 20:51:54 +0200

    sign up v0

--- app/src/main/java/com/example/piston/MainActivity.java
+++ app/src/main/java/com/example/piston/MainActivity.java
@@ -24,12 +24,8 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 }
