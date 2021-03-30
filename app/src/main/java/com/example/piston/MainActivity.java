27dffa370ec39ea7249cbbd9acaff1393ec1242e
Antonio Tigri <entertainmentatv@gmail.com>
2021-03-30 16:50:15 +0200

    Gradle Dependencies added

--- app/src/main/java/com/example/piston/MainActivity.java
+++ app/src/main/java/com/example/piston/MainActivity.java
@@ -16,9 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.create_post);
         setContentView(R.layout.activity_main);
-
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
