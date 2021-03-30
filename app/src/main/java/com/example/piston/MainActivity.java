672c318669e88616db938eae469950de25b2b53b
Chang Ye <1020994740a@gmail.com>
2021-03-30 18:01:22 +0200

    Cardview v0

--- app/src/main/java/com/example/piston/MainActivity.java
+++ app/src/main/java/com/example/piston/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        setContentView(R.layout.cardview_layout);
+        /*SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
@@ -26,6 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+                .setAction("Action", null).show());*/
     }
 }
