14569ac3a4cbcf1c5446f812ac96cc2702767c49
Antonio Tigri <entertainmentatv@gmail.com>
2021-04-03 23:13:57 +0200

    Layout Upgrades

--- app/src/main/java/com/example/piston/view/main/global/CreateCategoryActivity.java
+++ app/src/main/java/com/example/piston/view/main/global/CreateCategoryActivity.java
@@ -1,17 +1,40 @@
 package com.example.piston.view.main.global;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.piston.R;
 import com.example.piston.view.PistonActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateCategoryActivity extends PistonActivity {
+
+    ClipboardManager clipboard;
+    ClipData clip;
+    TextInputLayout link;
+    TextInputLayout desc;
 
     @Override
     protected void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_create_category);
+
+        clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        link = findViewById(R.id.category_link);
+
+        desc = findViewById(R.id.input_category_desc);
+        desc.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+
+        link.setEndIconOnClickListener(v -> {
+            clip = ClipData.newPlainText("CategoryCode", link.getEditText().getText());
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, R.string.link_copied, Toast.LENGTH_LONG).show();
+        });
+
     }
 
     public void createCategory(View view) {
--- app/src/main/java/com/example/piston/view/main/group/CreateGroupActivity.java
+++ app/src/main/java/com/example/piston/view/main/group/CreateGroupActivity.java
@@ -1,17 +1,38 @@
 package com.example.piston.view.main.group;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.piston.R;
 import com.example.piston.view.PistonActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateGroupActivity extends PistonActivity {
+
+    ClipboardManager clipboard;
+    ClipData clip;
+    TextInputLayout link;
+    TextInputLayout desc;
 
     @Override
     protected void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_create_group);
+        clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        link = findViewById(R.id.group_link);
+
+        desc = findViewById(R.id.input_group_desc);
+        desc.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+
+        link.setEndIconOnClickListener(v -> {
+            clip = ClipData.newPlainText("CategoryCode", link.getEditText().getText());
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, R.string.link_copied, Toast.LENGTH_LONG).show();
+        });
     }
 
     public void createGroup(View view) {
