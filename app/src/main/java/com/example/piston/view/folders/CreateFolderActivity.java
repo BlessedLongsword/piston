3101ec8de8dc26036dbacfdde106815385a54253
Antonio Tigri <entertainmentatv@gmail.com>
2021-04-04 02:29:24 +0200

    Some Layouts Upgraded:
    +Create post
    +Edit post
    +Create Folder
    +Edit Folder
    and other

--- app/src/main/java/com/example/piston/view/folders/CreateFolderActivity.java
+++ app/src/main/java/com/example/piston/view/folders/CreateFolderActivity.java
@@ -1,6 +1,7 @@
 package com.example.piston.view.folders;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 
@@ -8,19 +9,24 @@ import androidx.annotation.Nullable;
 
 import com.example.piston.R;
 import com.example.piston.view.PistonActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateFolderActivity extends PistonActivity {
+
+    TextInputLayout name;
+    TextInputLayout desc;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_folder);
+        name = findViewById(R.id.create_folder_name);
+        desc = findViewById(R.id.create_folder_description);
+        desc.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
     }
 
     public void createFolder(View view) {
-        EditText title = view.findViewById(R.id.input_folder_name);
-        EditText desc = view.findViewById(R.id.input_folder_desc);
-        pistonViewModel.setFolderChooser(title.getText().toString(), desc.getText().toString());
+        pistonViewModel.setFolderChooser(name.getEditText().getText().toString(), desc.getEditText().getText().toString());
         onBackPressed();
     }
 }
--- app/src/main/java/com/example/piston/view/posts/CreatePostActivity.java
+++ app/src/main/java/com/example/piston/view/posts/CreatePostActivity.java
@@ -1,6 +1,7 @@
 package com.example.piston.view.posts;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,18 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.piston.R;
 import com.example.piston.view.PistonActivity;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CreatePostActivity extends PistonActivity {
 
-    TextView title, content;
-    String picturePath;
+    TextInputLayout title;
+    TextInputLayout postContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
-        title = findViewById(R.id.card_title);
-        content = findViewById(R.id.card_content);
+        postContent = findViewById(R.id.post_content);
+        postContent.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
     }
 
    /* public void createPost(View view) {
