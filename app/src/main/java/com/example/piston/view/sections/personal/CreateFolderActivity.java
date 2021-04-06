e30511a18b2844dad2b6bb9938082f1c7c451de1
Andreu Vall <andreu.vallhernandez@gmail.com>
2021-04-07 00:34:19 +0200

    Solved crash when creating folder

--- app/src/main/java/com/example/piston/view/sections/personal/CreateFolderActivity.java
+++ app/src/main/java/com/example/piston/view/sections/personal/CreateFolderActivity.java
@@ -2,6 +2,7 @@ package com.example.piston.view.sections.personal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,7 +27,7 @@ public class CreateFolderActivity extends AppCompatActivity {
         //desc.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
     }
 
-    public void createFolder(View view) {
+    public void createFolder(MenuItem item) {
         personalFragmentViewModel.createFolder(title.getEditText().getText().toString(),
                 desc.getEditText().getText().toString());
         Intent output = new Intent();
@@ -36,60 +37,3 @@ public class CreateFolderActivity extends AppCompatActivity {
         finish();
     }
 }
-
-
-/*package com.example.piston.view.main.personal;
-
-import android.os.Bundle;
-import android.text.InputType;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.piston.R;
-import com.example.piston.viewmodel.PersonalFragmentViewModel;
-import com.google.android.material.textfield.TextInputLayout;
-
-public class CreateFolderActivity extends AppCompatActivity {
-
-    private PersonalFragmentViewModel personalFragmentViewModel;
-    private TextInputLayout title, desc;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_create_folder, container, false);
-        title = view.findViewById(R.id.create_folder_name);
-        desc = view.findViewById(R.id.create_folder_description);
-        desc.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
-        Button createFolderBtn = view.findViewById(R.id.create_folder_btn);
-        createFolderBtn.setOnClickListener(this);
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        personalFragmentViewModel = new ViewModelProvider(requireActivity()).get(PersonalFragmentViewModel.class);
-    }
-
-    @Override
-    public void onClick(View view) {
-        personalFragmentViewModel.createFolder(title.getEditText().getText().toString(),
-                desc.getEditText().getText().toString());
-        dismiss();
-    }
-}
-*/
