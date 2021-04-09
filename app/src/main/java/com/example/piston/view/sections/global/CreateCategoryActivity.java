54e94f82ede3452b827f788544aa7015130af1e4
Guillermo <guillermomariscalvizcaya@gmail.com>
2021-04-09 20:38:25 +0200

    create category activity

--- app/src/main/java/com/example/piston/view/sections/global/CreateCategoryActivity.java
+++ app/src/main/java/com/example/piston/view/sections/global/CreateCategoryActivity.java
@@ -2,29 +2,37 @@ package com.example.piston.view.sections.global;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.piston.R;
+import com.example.piston.viewmodel.GlobalFragmentViewModel;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateCategoryActivity extends AppCompatActivity {
 
     ClipboardManager clipboard;
     ClipData clip;
-    TextInputLayout link, desc;
+    TextInputLayout link, desc, title;
+    private GlobalFragmentViewModel globalFragmentViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstances) {
         super.onCreate(savedInstances);
         setContentView(R.layout.activity_create_category);
 
+        globalFragmentViewModel = new ViewModelProvider(this).get(GlobalFragmentViewModel.class);
+
         clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         link = findViewById(R.id.category_link);
+
+        title = findViewById(R.id.input_category_name);
 
         desc = findViewById(R.id.input_category_desc);
         desc.getEditText().setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
@@ -38,6 +46,12 @@ public class CreateCategoryActivity extends AppCompatActivity {
     }
 
     public void createCategory(MenuItem item) {
+        globalFragmentViewModel.createCategory(title.getEditText().getText().toString(), desc.getEditText().getText().toString());
+        Intent output = new Intent();
+        output.putExtra("title", title.getEditText().getText().toString());
+        output.putExtra("desc", desc.getEditText().getText().toString());
+        output.putExtra("link", link.getEditText().getText().toString());
+        setResult(RESULT_OK, output);
         finish();
     }
 }
