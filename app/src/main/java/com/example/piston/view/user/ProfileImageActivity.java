0acc4d3b54b680bf0152d38527daa30f1bb99318
Chang Ye <1020994740a@gmail.com>
2021-04-11 21:28:56 +0200

    view profile edit buttons

---
+++ app/src/main/java/com/example/piston/view/user/ProfileImageActivity.java
@@ -0,0 +1,6 @@
+package com.example.piston.view.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileImageActivity extends AppCompatActivity  {
+}
--- app/src/main/java/com/example/piston/view/user/ViewProfileActivity.java
+++ app/src/main/java/com/example/piston/view/user/ViewProfileActivity.java
@@ -1,31 +1,26 @@
 package com.example.piston.view.user;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.example.piston.R;
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class ViewProfileActivity extends AppCompatActivity {
@@ -39,11 +34,74 @@ public class ViewProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
-        layoutParams.dimAmount = 0.75f;
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-        getWindow().setAttributes(layoutParams);
         init();
+    }
+
+    public void clickImage(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void popUpWindow(View anchorView, EditText edit) {
+
+        View popupView = getLayoutInflater().inflate(R.layout.reply_post_youtube, null);
+
+        PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        // If you need the PopupWindow to dismiss when touched outside
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+        popupWindow.setAnimationStyle(R.style.popup_window_animation_slide);
+        // Adjust popup window location when keyboard pops up :)))
+        popupWindow.setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
+        popupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        // Using location, the PopupWindow will be displayed right under anchorView
+        popupWindow.showAtLocation(anchorView, Gravity.BOTTOM, 0, 0);
+
+        // Initialize objects from layout
+        TextInputLayout ed = popupView.findViewById(R.id.popup);
+        ed.getEditText().setText(edit.getText().toString());
+        ed.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(ed.getEditText().getText().toString().length() > 0) {
+                    ed.setEndIconMode(TextInputLayout.END_ICON_CUSTOM);
+                    ed.setEndIconDrawable(R.drawable.outline_send_black_36);
+                }
+                else {
+                    ed.setEndIconMode(TextInputLayout.END_ICON_NONE);
+                }
+            }
+        });
+
+        ed.getEditText().requestFocus();
+        popupWindow.setOutsideTouchable(false);
+
+        // force show keyboar once pop up window is open
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
+
+
+        // dims background when popup window shows up
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha=0.5f;
+        getWindow().setAttributes(lp);
+
+        // restore dim
+        popupWindow.setOnDismissListener(() -> {
+            lp.alpha=1f;
+            getWindow().setAttributes(lp);
+        });
     }
 
     public void cancel (View v) {
@@ -123,72 +181,6 @@ public class ViewProfileActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }*/
-    }
-
-    public void clickImage(View view){
-        Toast.makeText(getApplicationContext(),"CLICKED", Toast.LENGTH_SHORT).show();
-    }
-
-    public void popUpWindow(View anchorView, EditText edit) {
-
-        View popupView = getLayoutInflater().inflate(R.layout.reply_post_youtube, null);
-
-        PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT, true);
-        // If you need the PopupWindow to dismiss when touched outside
-        popupWindow.setBackgroundDrawable(new ColorDrawable());
-        popupWindow.setAnimationStyle(R.style.popup_window_animation_slide);
-        // Adjust popup window location when keyboard pops up :)))
-        popupWindow.setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
-        popupWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
-
-        // Using location, the PopupWindow will be displayed right under anchorView
-        popupWindow.showAtLocation(anchorView, Gravity.BOTTOM, 0, 0);
-
-        // Initialize objects from layout
-        TextInputLayout ed = popupView.findViewById(R.id.popup);
-        ed.getEditText().setText(edit.getText().toString());
-        ed.getEditText().addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if(ed.getEditText().getText().toString().length() > 0) {
-                    ed.setEndIconMode(TextInputLayout.END_ICON_CUSTOM);
-                    ed.setEndIconDrawable(R.drawable.outline_send_black_36);
-                }
-                else {
-                    ed.setEndIconMode(TextInputLayout.END_ICON_NONE);
-                }
-            }
-        });
-
-        ed.getEditText().requestFocus();
-        popupWindow.setOutsideTouchable(false);
-
-        // force show keyboar once pop up window is open
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
-
-
-        // dims background when popup window shows up
-        WindowManager.LayoutParams lp = getWindow().getAttributes();
-        lp.alpha=0.5f;
-        getWindow().setAttributes(lp);
-
-        // restore dim
-        popupWindow.setOnDismissListener(() -> {
-            lp.alpha=1f;
-            getWindow().setAttributes(lp);
-        });
     }
 
 }
