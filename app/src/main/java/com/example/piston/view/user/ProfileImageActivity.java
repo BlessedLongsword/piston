e6c1751b16b558eae463eb9329d7cf0883b4c0b6
Chang Ye <1020994740a@gmail.com>
2021-04-12 01:00:04 +0200

    view profile image activity + zoom (not dragable)

--- app/src/main/java/com/example/piston/view/user/ProfileImageActivity.java
+++ app/src/main/java/com/example/piston/view/user/ProfileImageActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,15 +15,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.piston.R;
 
 public class ProfileImageActivity extends AppCompatActivity  {
-
+    private ScaleGestureDetector scaleGestureDetector;
+    private float mScaleFactor = 1.0f;
     ImageView picture;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_image);
         picture = findViewById(R.id.picture);
         Bitmap bitmap = BitmapFactory.decodeByteArray(
                 getIntent().getByteArrayExtra("byteArray"), 0, getIntent().getByteArrayExtra("byteArray").length);
         picture.setImageBitmap(bitmap);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            picture.setScaleX(mScaleFactor);
+            picture.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/piston/view/user/ViewProfileActivity.java
+++ app/src/main/java/com/example/piston/view/user/ViewProfileActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
