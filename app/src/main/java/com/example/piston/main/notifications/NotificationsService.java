c1a1d126051461d870e03d747dea4bb86f408a8b
Antonio Tigri <entertainmentatv@gmail.com>
2021-05-27 12:46:10 +0200

    Notifications Upgrade

--- app/src/main/java/com/example/piston/main/notifications/NotificationsService.java
+++ app/src/main/java/com/example/piston/main/notifications/NotificationsService.java
@@ -5,16 +5,33 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.IBinder;
+import android.provider.MediaStore;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.piston.R;
+import com.example.piston.data.notifications.Notification;
 import com.example.piston.data.notifications.NotificationPost;
 import com.example.piston.data.notifications.NotificationReply;
+import com.example.piston.main.posts.PostActivity;
 import com.example.piston.main.settings.SettingsActivity;
 import com.example.piston.utilities.Values;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,12 +40,14 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ListenerRegistration;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class NotificationsService extends Service {
 
     private boolean first = true;
     private ListenerRegistration listener;
+    private Bitmap sectionImage;
 
     @Nullable
     @Override
@@ -100,41 +119,118 @@ public class NotificationsService extends Service {
                         if (notificationType.equals("post")) {
                             NotificationPost notification = documentSnapshot
                                     .toObject(NotificationPost.class);
-                            postNotification(notification.getTitle(), notification.getSectionID(),
-                                    documentSnapshot.getId());
+                            Glide.with(this).asBitmap()
+                                    .load(notification.getContextImageLink())
+                                    .into(new CustomTarget<Bitmap>() {
+                                        @Override
+                                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                            postNotificationPost(notification, resource, getCircleBitmap(sectionImage));
+                                        }
+                                        @Override
+                                        public void onLoadCleared(@Nullable Drawable placeholder) {
+                                        }
+                                    });
                         } else {
                             NotificationReply notification = documentSnapshot
                                     .toObject(NotificationReply.class);
-                            postNotification(notification.getUser(), notification.getContent(),
-                                    documentSnapshot.getId());
+                            Glide.with(this).asBitmap()
+                                    .load(notification.getContextImageLink())
+                                    .into(new CustomTarget<Bitmap>() {
+                                @Override
+                                public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                    postNotificationReply(notification, getCircleBitmap(resource));
+                                }
+                                @Override
+                                public void onLoadCleared(@Nullable Drawable placeholder) {
+                                }
+                            });
                         }
                     }
                 });
     }
 
-    public void postNotification(String title, String content, String id) {
+    public void postNotificationPost(NotificationPost notification, Bitmap postImage, Bitmap sectionImage) {
 
-        PendingIntent contentIntent =
-                PendingIntent.getActivity(this, 0, new Intent(this,
-                        NotificationsActivity.class), 0);
+        Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+        intent.putExtra(Values.SCOPE, notification.getScope());
+        intent.putExtra(Values.SECTION_ID, notification.getSectionID());
+        intent.putExtra(Values.POST_ID, notification.getPostID());
+        intent.putExtra(Values.ORPHAN, true);
+
+        PendingIntent contentIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.mipmap.logo)
-                .setContentTitle(title)
-                .setContentText(content)
+                .setSmallIcon(R.drawable.outline_post_add_black_24)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getSectionName())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setLargeIcon(sectionImage)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(postImage)
+                        .bigLargeIcon(sectionImage))
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
         // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(id.hashCode(), builder.build());
+        notificationManager.notify(notification.getNotificationID().hashCode(), builder.build());
+    }
+
+    public void postNotificationReply(NotificationReply notification, Bitmap userImage) {
+
+        Intent intent = new Intent(getApplicationContext(), PostActivity.class);
+        intent.putExtra(Values.SCOPE, notification.getScope());
+        intent.putExtra(Values.SECTION_ID, notification.getSectionID());
+        intent.putExtra(Values.POST_ID, notification.getPostID());
+        intent.putExtra(Values.ORPHAN, true);
+
+        PendingIntent contentIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.outline_reply_white_24)
+                .setContentTitle(notification.getUser() + " " + getString(R.string.replied))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setLargeIcon(userImage)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(notification.getContent()))
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notification.getNotificationID().hashCode(), builder.build());
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         listener.remove();
+    }
+
+    private Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return output;
     }
 }
